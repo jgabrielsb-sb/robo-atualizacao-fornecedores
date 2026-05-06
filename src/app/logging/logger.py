e9c770f9761071c5e5e082cf7c4971c8091635d8
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2026-05-06 15:44:59 -0300

    feat: adding event name and status on loging json

--- src/app/logging/logger.py
+++ src/app/logging/logger.py
@@ -2,3 +2,7 @@ import logging.config
 from config.logging_settings import logging_settings
 
 logging.config.dictConfig(logging_settings)
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.error("errojkjkjkr", extra={"event_name": "TESTE_EVENT", "status": "ERROR"})
