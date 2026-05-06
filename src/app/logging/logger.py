03aa4a2b56a096d76757da0c8b425b4c049b6bb3
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2026-05-06 15:35:24 -0300

    chore

--- src/app/logging/logger.py
+++ src/app/logging/logger.py
@@ -2,11 +2,3 @@ import logging.config
 from config.logging_settings import logging_settings
 
 logging.config.dictConfig(logging_settings)
-logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    try:
-        result = 1 / 0
-    except Exception as e:
-        logger.error("error", exc_info=e)
-    
