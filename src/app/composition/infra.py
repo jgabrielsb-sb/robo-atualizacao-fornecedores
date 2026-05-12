36a4908d095060547cedebb07f0d86d50146f2ad
Gabriel Sampaio de Barros <jgabrielsb2002@gmail.com>
2026-05-12 12:03:36 -0300

    feat: fornecedores api requester

--- src/app/composition/infra.py
+++ src/app/composition/infra.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 
+from config.settings import settings
+from app.infra.api_requester.fornecedores_api_requester import FornecedoresAPIRequester
 
 @dataclass
 class InfraProvider:
-    pass
+    def get_fornecedores_api_requester(self) -> FornecedoresAPIRequester:
+        return FornecedoresAPIRequester(
+            base_url=settings.FORNECEDORES_API_BASE_URL
+        )
+
+    
