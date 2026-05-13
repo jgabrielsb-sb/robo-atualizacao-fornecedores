c9070a8ed974ea1511ad5b72d8179c1b09933445
Gabriel Sampaio de Barros <jgabrielsb2002@gmail.com>
2026-05-13 11:37:47 -0300

    chore

--- src/app/application/ports/fornecedor/__init__.py
+++ src/app/application/ports/fornecedor/__init__.py
@@ -1,3 +1,7 @@
 from .fornecedor_repository_port import FornecedorRepositoryPort
-from .build_fornecedor import BuildFornecedorPort, GetCartaoCNPJPort
-from .build_fornecedor import GetOptSimplesNacionalPort
+from .build_fornecedor import (
+    BuildFornecedorPort, 
+    GetCartaoCNPJPort, 
+    GetOptSimplesNacionalPort, 
+    GetEnderecoPort,
+)
--- src/app/application/ports/fornecedor/build_fornecedor/get_cartao_cnpj_port.py
+++ src/app/application/ports/fornecedor/build_fornecedor/get_cartao_cnpj_port.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 
 from app.domain.value_objects import CartaoCNPJ
+from app.domain.value_objects import CNPJ
 
 class GetCartaoCNPJPort(ABC):
     @abstractmethod
-    def get(self, cnpj: str) -> CartaoCNPJ:
+    def get(self, cnpj: CNPJ) -> CartaoCNPJ:
         pass
