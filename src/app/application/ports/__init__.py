4ccdbd21195ae300eb2dddfaa9104524911182f4
Gabriel Sampaio de Barros <jgabrielsb2002@gmail.com>
2026-05-28 14:47:34 -0300

    feat: get atividade economica description port and adapter via fornecedores api requester

--- src/app/application/ports/__init__.py
+++ src/app/application/ports/__init__.py
@@ -9,3 +9,4 @@ from .fornecedor import (
 )
 
 from .municipio_lookup_port import MunicipioLookupPort
+from .get_atividade_economica_description_port import GetAtividadeEconomicaDescriptionPort
---
+++ src/app/infra/adapters/get_atividade_economica_description/__init__.py
@@ -0,0 +1,5 @@
+from .get_atividade_economica_description_via_fornecedores_api import GetAtividadeEconomicaDescriptionViaFornecedoresAPI
+
+__all__ = [
+    "GetAtividadeEconomicaDescriptionViaFornecedoresAPI",
+]
